330da7ace227303c0484841ea6599b3a0711525b
david <david@10.240.81.233>
2018-03-01 13:59:06 -0500

    h

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -10,6 +10,19 @@ public class hello {
 		System.out.println("Hello github");
 		System.out.println("Hello github");
 		System.out.println("Did this work");
+		int[][] array = new int[10][];
+		array[5][] = 56;
 		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		if(value==1) {
+			one=" ";
+		}else if(value1==2) {
+			two=" ";
+		}
+		for(int i =1;i<=12;i++) {
+			System.out.print(one+"\t"+two);
+		}
 	}
 }
