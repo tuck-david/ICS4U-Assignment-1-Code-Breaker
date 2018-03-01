758e6a187ff80889c1017292205e5c7256e076cd
david <david@10.240.81.233>
2018-03-01 13:27:15 -0500

    yolo

---
+++ test/src/test/hello.java
@@ -0,0 +1,21 @@
+/**
+ * Author: David Tuck
+ * Date: Mar 1, 2018
+ */
+package test;
+
+/**
+ * @author david
+ *
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello github");
+	}
+
+}
