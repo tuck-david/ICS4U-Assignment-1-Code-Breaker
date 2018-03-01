0adf5f75e1f90f7cedfb1c6dd5943c546af569ea
david <david@10.240.81.233>
2018-03-01 13:28:52 -0500

    wrethrytuyj

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -4,17 +4,9 @@
  */
 package test;
 
-/**
- * @author david
- *
- */
 public class hello {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("Hello github");
 		System.out.println("Hello github");
 	}
