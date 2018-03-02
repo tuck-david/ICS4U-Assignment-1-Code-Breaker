d4fc2cedbd0fcff7da1f44eba7e98f221f6da6e1
tuck-david <32722002+tuck-david@users.noreply.github.com>
2018-03-01 22:00:29 -0500

    a

--- code_Breaker.java
+++ code_Breaker.java
@@ -1,38 +1,36 @@
 /**
- * Author: David Tuck and Abdalla
- * Date: Mar 1, 2018
+ * Author: David Tuck and Abdalla Date: Mar 1, 2018
  */
-package test;
 
 public class code_Breaker {
-	public static void main(String[] args) {//David and Abdalla
-		final Int SIZE = 10;//max Number of guesses
+	public static void main(String[] args) {// David and Abdalla
+		final int SIZE = 10;// max Number of guesses
 		final String VALID_CHARS = ("GRBYOP");
-		System.out.print(VALID_CHARS.legth);
+		System.out.print(VALID_CHARS.length());
 	}
 
-	public static void createCode() {//Abdalla
-
-	}
-
-	public static void valid() {//Abdalla
-
+	public static void createCode() {// Abdalla
 
 	}
 
-	public static void findFullyCorrect() {//Abdalla
+
+	public static void valid() {// Abdalla
 
 	}
 
-	public static void removeFullyCorrect() {//David
+	public static void findFullyCorrect() {// Abdalla
 
 	}
 
-	public static void findColourCorrect() {//David
+	public static void removeFullyCorrect() {// David
 
 	}
 
-	public static void displayGame() {//David
+	public static void findColourCorrect() {// David
+
+	}
+
+	public static void displayGame() {// David
 
 	}
 
