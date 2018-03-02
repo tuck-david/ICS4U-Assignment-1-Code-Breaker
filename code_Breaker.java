89bdcb22b41bca4541d9952475236cfda9f404f3
tuck-david <32722002+tuck-david@users.noreply.github.com>
2018-03-01 22:14:09 -0500

    worked on method 5

--- code_Breaker.java
+++ code_Breaker.java
@@ -6,13 +6,12 @@ public class code_Breaker {
 	public static void main(String[] args) {// David and Abdalla
 		final int SIZE = 10;// max Number of guesses
 		final String VALID_CHARS = ("GRBYOP");
-		System.out.print(VALID_CHARS.length());
+
 	}
 
 	public static void createCode() {// Abdalla
 
 	}
-
 
 	public static void valid() {// Abdalla
 
@@ -22,8 +21,17 @@ public class code_Breaker {
 
 	}
 
-	public static void removeFullyCorrect() {// David
-
+	public static void removeFullyCorrect(String[] array1, String[] array2) {// David
+		int[] returnArray = new int[array1.length];
+		int c = 0;
+		for (int i = 0; i < array1.length; i++) {
+			for (int a = 0; a < array1.length; a++) {
+				if (array1[i] == array2[a]) {
+					returnArray[c] = Integer.parseInt(array1[i]);
+					c++;
+				}
+			}
+		}
 	}
 
 	public static void findColourCorrect() {// David
