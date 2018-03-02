cea6c0a71351f34f1db95d3e7e40b9fc544442ae
IamAbdu <36971595+IamAbdu@users.noreply.github.com>
2018-03-02 11:54:58 -0500

    Update Abdalla-code-Breaker.java

--- Abdalla-code-Breaker.java
+++ Abdalla-code-Breaker.java
@@ -1,1 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ * Author: David Tuck and Abdalla Date: Mar 1, 2018
+ */
+
+public class code_Breaker {
+	public static void main(String[] args) {// David and Abdalla
+		final int SIZE = 4;// max Number of guesses
+		final int TRIES = 10;
+		final String VALID_CHARS = ("GRBYOP");
