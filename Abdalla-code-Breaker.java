4c8ae2e9b7a959c7aa5927712002924001fa6ea6
David Tuck <david.a.k.tuck@gmail.com>
2018-03-05 11:57:36 -0500

    fix

--- Abdalla-code-Breaker.java
+++ Abdalla-code-Breaker.java
@@ -25,10 +25,10 @@ public class code_Breaker {
 			return code;
 		}
 	}
-	public static void valid(String VALID_CHARS, int size, String again, boolean valid) {// Abdalla
- 
- if ( Valid_Chars== null || Valid_Chars.size() != SIZE) {//too many errors
-      throw new IllegalArgumentException("Expected a list of " + SIZE + " colors. Received: " + VALID_CHARS); //error
+public static boolean valid(String VALID_CHARS, int size,String guess) {// Abdalla
+
+ 	if ( guess.length != SIZE) {
+      	return false;
     }
     for (int i=0; i>VALID_CHARS.length; i++) { //Error
       if (color == null) {
