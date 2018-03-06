dfca1a28cfc5df44f686de38d7f0a23afb068b78
IamAbdu <36971595+IamAbdu@users.noreply.github.com>
2018-03-06 17:04:58 -0500

    Update Abdalla-code-Breaker.java

--- Abdalla-code-Breaker.java
+++ Abdalla-code-Breaker.java
@@ -1,34 +1,113 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-/**
- * Author: David Tuck and Abdalla Date: Mar 1, 2018
- */
-
 public class code_Breaker {
+	static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {// David and Abdalla
-		final int SIZE = 4;// max Number of guesses
+		final int SIZE = 4;
 		final int TRIES = 10;
+		boolean playAgain = false;
+
 		final String VALID_CHARS = ("GRBYOP");
-		public static String[] createCode(String VALID_CHARS, int size) {// Abdalla
-			char[] chars = new char[VALID_CHARS.length() - 1];
-			for (int i = 0; i < VALID_CHARS.length() - 1; i++) {
-				chars[i] = VALID_CHARS.charAt(i);
+		do {// Beginning of new game
+			int tryCount = 0;// Counts how many guesses user has made
+			boolean exit = false;
+			String[][] guessHistory = new String[TRIES][SIZE];// 2d array the stores all guesses
+			String[][] answerHistory = new String[TRIES][VALID_CHARS.length()];
+			playAgain = true;
+			String[] genCode = createCode(VALID_CHARS, SIZE);// genCode is the code the user is trying to guess
+			do {// loop for each guess of a game
+
+				if (tryCount >= SIZE - 1) {
+					System.out.println("I'm sorry you lose. The correct code was " + Arrays.toString(genCode));
+
+					exit = true;
+				} else {
+					System.out.println(displayGame(guessHistory, answerHistory));
+					String[] currentGuess = getinput(SIZE, VALID_CHARS, tryCount, guessHistory);
+					if (currentGuess == genCode) {
+						System.out.println("Congratulations! it took you " + (tryCount + 1) + " to find the code");
+						exit = true;
+						break;
+					}
+					String[] fullyCorrectAns = findFullyCorrect(genCode, currentGuess);
+					currentGuess = removeFullyCorrect(genCode, currentGuess);
+					String[] colourCorrctAns = findColourCorrect(genCode, currentGuess);
+					List<String> answerHistoryList = new ArrayList<String>();
+					for (int i = 0; i < SIZE; i++) {
+						answerHistory[tryCount][i] = fullyCorrectAns[i];
+					}
+					for (int i = 0; i < SIZE; i++) {
+						answerHistory[tryCount][i] = colourCorrctAns[i];
+					}
+
+				}
+				tryCount++;
+			} while (exit == false);
+			String playAgainAnswer;
+			do {// do loop is for looping the game so you can play more than one game
+				System.out.println("Do you want to play again. y-yes n-no");
+				playAgainAnswer = input.nextLine();
+			} while (valid(("yn"), 1, playAgainAnswer) == false);
+			if (playAgainAnswer.equals("n")) {
+				playAgain = false;
 			}
-			String[] code = new String[size];
+		} while (playAgain = true);
+
+	}
+
+	public static String[] createCode(String VALID_CHARS, int size) {// Abdalla
+		char[] chars = new char[VALID_CHARS.length() - 1];
+		for (int i = 0; i < VALID_CHARS.length() - 1; i++) {
+			chars[i] = VALID_CHARS.charAt(i);
+		}
+		String[] code = new String[size];
+		for (int i = 0; i < size; i++) {
+			code[i] = Character.toString(VALID_CHARS.charAt((int) (Math.random() * 6)));
+		}
+
+		return code;
+	}
+
+	public static String[] getinput(int size, String valid_chars, int tryCount, String[][] guessHistory) {
+		boolean valid = true;
+		char[] chars = new char[valid_chars.length() - 1];
+		String answer;
+		String again = " ";
+		String[] currectAnswer = new String[size];
+		do {
+			valid = true;
+			System.out.print(
+					"Please enter your guess" + again + "of length " + (size) + " using the letters " + valid_chars);
+			System.out.println();
+			answer = input.nextLine();
+
+			if (valid(valid_chars, size, answer) == false) {
+				valid = false;
+				again = " again ";
+			}
+
+			for (int i = 0; i < answer.length(); i++) {// converts string to single char arrays
+				currectAnswer[i] = Character.toString(answer.charAt(i));
+			}
+		} while (valid == false);
+
+		return currectAnswer;
+	}
+
+	public static boolean valid(String VALID_CHARS, int size, String guess) {// Abdalla
+		boolean guessHas = true;
+		if (guess.length() != size) {
+			return false;
+		} else {
 			for (int i = 0; i < size; i++) {
-				code[i] = Character.toString(VALID_CHARS.charAt((int) (Math.random() * 6)));
+				if (!VALID_CHARS.contains(Character.toString(guess.charAt(i)))) {
+					guessHas = false;
+				}
+
 			}
-			System.out.println(Arrays.toString(code));
-			return code;
+			return guessHas;
 		}
 	}
-public static boolean valid(String VALID_CHARS, int size,String guess) {// Abdalla
 
- 	if ( guess.length != SIZE) {
-      	return false;
-    }
-
-}
+	public static String[] findFullyCorrect(String[] gencode, String[] currentGuess) {// Abdalla
+		return gencode;// Too change
+	}
