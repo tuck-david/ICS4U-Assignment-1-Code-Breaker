8b1ce9dd93e9cfe5c44fb611d2c66547488e696a
IamAbdu <36971595+IamAbdu@users.noreply.github.com>
2018-03-07 12:09:54 -0500

    Update code_Breaker.java

--- code_Breaker.java
+++ code_Breaker.java
@@ -122,7 +122,19 @@ public class code_Breaker {
 		return code;
 	}
 
-	public static String[] getinput(int size, String valid_chars, int tryCount, String[][] guessHistory) {
+	/**
+	* Allows the user to input their guess of the code that was randomly generated. 
+	*
+	* @param VALID_CHARS  the colors that the user would have to use when inputting
+	*                     their guess
+	*        SIZE         the size/length of the code that the user must guess      
+	* @return             the guess of the user, if the guess was incorrect the 
+	*                     user will be able to guess once again, until the last 
+	*                     attempt, or guess the correct code that was generated
+	*                    
+	*/ 
+	
+	public static String[] getinput(int size, String valid_chars ) {
 		boolean valid = true;
 		char[] chars = new char[valid_chars.length() - 1];
 		String answer;
