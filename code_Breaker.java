ac0624caac98e89da70b57ba8c7c9562b3b0cde9
David Tuck <david.a.k.tuck@gmail.com>
2018-03-08 19:36:51 -0500

    Fixes to java docs and comments

--- code_Breaker.java
+++ code_Breaker.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Date: Mar 1, 2018 Assignment: Code Breaker
@@ -14,61 +15,64 @@ public class code_Breaker {
 	static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {// David
-		final int SIZE = 4;
-		final int TRIES = 10;
-		boolean playAgain = false;
-		final String VALID_CHARS = ("GRBYOP");
+		final int SIZE = 5;// The size of the code that is trying to guessed
+		final int TRIES = 10;// The number of Guesses the user has to guess the code in one game
+		boolean playAgain = false;// Variable to control if a game is to be played again or not
+		final String VALID_CHARS = ("GRBYOP");// String that defines all acceptable colors that are to be used
 		System.out.println("Hello and Welcome to the code breaker game");
 		do {// Beginning of new game
-			System.out.println(
-					"If you would like to see the rules please enter 'r' now. Enter anything else to continue");
-			if (input.nextLine().equalsIgnoreCase("r")) {
+			System.out.println("If you would like to see the rules please enter 'r' now. Enter anything else to continue");
+			if (input.nextLine().equalsIgnoreCase("r")) {// checks to see if user wants to see the rules
 				rules(VALID_CHARS, SIZE, TRIES);
 			}
 			System.out.println("\t**********NEW GAME**********");
 			int tryCount = 0;// Counts how many guesses user has made
-			boolean exit = false;
+			boolean exit = false;// boolean to track if the game should end prematurely
 			String[][] guessHistory = new String[TRIES][SIZE];// 2d array the stores all guesses
-			String[][] answerHistory = new String[TRIES][VALID_CHARS.length()];
-			for (String[] strings : answerHistory) {// fills array so there is no null
+			String[][] answerHistory = new String[TRIES][VALID_CHARS.length()];// 2D array that stores all answers
+			for (String[] strings : answerHistory) {// fills answerHistory array so there is no null
 				Arrays.fill(strings, "");
 			}
-			for (String[] strings : guessHistory) {// fills array so there is no null
+			for (String[] strings : guessHistory) {// fills guessHistory array so there is no null
 				Arrays.fill(strings, "");
 			}
-			playAgain = false;
+			playAgain = false;// playAgain is set to false so game is not looped infinitely
 			String[] genCode = createCode(VALID_CHARS, SIZE);// genCode is the code the user is trying to guess
 
 			do {// loop for each guess of a game
 				String[] genCodeCOPY = genCode.clone();
-				if (tryCount >= TRIES) {
+				if (tryCount >= TRIES) {// checks to see if user has used all of there guesses
 					System.out.println("I'm sorry you lose. The correct code was " + Arrays.toString(genCode));
 					exit = true;
 				} else {
-					int writePos = 0;
+					// code for advancing the state of the game
 					System.out.println(Arrays.toString(genCode));
-					System.out.println(displayGame(guessHistory, answerHistory, tryCount));
-					String[] currentGuess = getinput(SIZE, VALID_CHARS);
-					for (int i = 0; i < SIZE; i++) {
+					System.out.println(displayGame(guessHistory, answerHistory, tryCount));// displays the game-board
+					String[] currentGuess = getInput(SIZE, VALID_CHARS);// after game is display user call getInput
+					for (int i = 0; i < SIZE; i++) {// adds to the guessHistory array in the row of the current tryCount
 						guessHistory[tryCount][i] = currentGuess[i];
 					}
 					String[] fullyCorrectAns = findFullyCorrect(genCode, currentGuess);
-					if (fullyCorrectAns.length == SIZE) {
-						System.out
-								.println("Congratulations! it took you " + (tryCount + 1) + " guess to find the code");
+					// fullyCorrectAns is the "b" component the the clues. The size of
+					// fullyCorrectAns is exactly equal to the number of "b"
+					if (fullyCorrectAns.length == SIZE) {// if the fullyCorrectAns is the same size as SIZE than the
+															// user has guessed the correct code
+						System.out.println("Congratulations! it took you " + (tryCount + 1) + " guess to find the code");
 						exit = true;
 						break;
 					}
 					currentGuess = removeFullyCorrect(genCodeCOPY, currentGuess);
+					// fullyCorrectAns is the "w" component the the clues. The size of
+					// fullyCorrectAns is exactly equal to the number of "w"
 					String[] colourCorrctAns = findColourCorrect(genCodeCOPY, currentGuess);
-
-					for (int i = 0; i < fullyCorrectAns.length; i++) {
+					int writePos = 0;// Variable to track where i have written to in the aswerHistory array
+					for (int i = 0; i < fullyCorrectAns.length; i++) {// adds fullyCorrectAns to tryCount row in aswerHistory
 						answerHistory[tryCount][writePos] = fullyCorrectAns[i];
-						writePos++;
+						writePos++;// wrtiePos is ++ so that the position of the next clue is in the next index
 					}
-					for (int i = 0; i < colourCorrctAns.length; i++) {
+					for (int i = 0; i < colourCorrctAns.length; i++) {// adds colourCorrctAns to tryCount row in aswerHistory
 						answerHistory[tryCount][writePos] = colourCorrctAns[i];
-						writePos++;
+						writePos++;// wrtiePos is ++ so that the position of the next clue is in the next index
 					}
 				}
 				tryCount++;
@@ -77,88 +81,92 @@ public class code_Breaker {
 			do {// do loop is for looping the game so you can play more than one game
 				System.out.println("Do you want to play again. press y for yes and press n for no");
 				playAgainAnswer = input.nextLine();
-			} while (valid(("yn"), 1, playAgainAnswer) == false);
+			} while (valid(("yn"), 1, playAgainAnswer) == false);// if user enters "r" a new game is started
 			if (playAgainAnswer.equalsIgnoreCase("y")) {
 				playAgain = true;
 			}
-		} while (playAgain == true);
+		} while (playAgain == true);// Loop for multiple games
 		System.out.println("Thank you for playing code breker we hope you come back soon!!!");
 	}
 
 	/**
-	 * Generates a size of 4 random color code from the 6 colors that is provided,
-	 * that the player has to guess in the game.
+	 * Generates genCode. createCode creates a array with length SIZE using the
+	 * characters contained in VALID_CHARS
 	 *
 	 * @param VALID_CHARS
-	 *            using the colors in the array to generate the code
+	 *            Colors that the user would have to use when inputting their guess
+	 *            each character in the string represents a different color. Only
+	 *            characters in this string are used to make the code out of.
 	 * @param SIZE
-	 *            element of the size/length of the code generated
-	 * @return the code generated by math.random * 6, that would contain four of the
-	 *         six colors in the list duplicated or not
+	 *            The SIZE/length of the code that the user must guess.
+	 * @return The variable code is an array that contains the randomly generated
+	 *         code that the user in trying to guess. Each index of the array holds
+	 *         one character that represents a color.
 	 */
 
 	public static String[] createCode(String VALID_CHARS, int SIZE) {// Abdalla
-		char[] chars = new char[VALID_CHARS.length() - 1];
-		for (int i = 0; i < VALID_CHARS.length() - 1; i++) {
-			chars[i] = VALID_CHARS.charAt(i);
-		}
-		String[] code = new String[SIZE];
+		String[] code = new String[SIZE];// Array to hold the randomly generated code
 		for (int i = 0; i < SIZE; i++) {
 			code[i] = Character.toString(VALID_CHARS.charAt((int) (Math.random() * 6)));
+			// Above code sets the i index to a random character of VALID_CHARS
 		}
 		return code;
 	}
 
 	/**
-	 * Allows the user to input their guess of the code that was randomly generated.
+	 * Allows the user to input their guess of the code.
 	 *
 	 * @param VALID_CHARS
-	 *            the colors that the user would have to use when inputting their
-	 *            guess
+	 *            Colors that the user would have to use when inputting their guess,
+	 *            each character in the string represents a different color. Only
+	 *            characters in this string are used to make the code out of.
 	 * @param SIZE
-	 *            the SIZE/length of the code that the user must guess
-	 * @return the guess of the user, if the guess was incorrect the user will be
-	 *         able to guess once again, until the last attempt, or guess the
-	 *         correct code that was generated
-	 *
+	 *            The SIZE/length of the code that the user must guess.
+	 * @return The guess of the user, if the guess was incorrect the user will be
+	 *         able to guess again, until the last attempt, or the user guesses the
+	 *         correct code that was generated. currectAnswer is evaluated in other
+	 *         methods to check for correctness compared to genCode.
 	 */
 
-	public static String[] getinput(int SIZE, String VALID_CHARS) {// David
-		boolean valid = true;
-		String currentGuess;
-		String again = " ";
+	public static String[] getInput(int SIZE, String VALID_CHARS) {// David
+		boolean valid = true;// boolean that is to be returned
+		String currentGuess;// String to hold the current guess of the user
+		String again = " ";// String to change what the system outputs
 		String[] currectAnswer = new String[SIZE];
 		do {
 			valid = true;
-			System.out.print("Please enter your guess" + again + "with a length of " + (SIZE)
-					+ " using the capital letters " + VALID_CHARS);
+			System.out.print("Please enter your guess" + again + "with a length of " + (SIZE) + " using the capital letters "
+					+ VALID_CHARS);
 			System.out.println();
 			currentGuess = input.nextLine();
 			if (valid(VALID_CHARS, SIZE, currentGuess) == false) {
-				valid = false;
+				// If the method, valid, returns true the currectAnswer is loaded in to an array
+				// to be returned, else the method loops and asks for the user to enter the
+				// guess again.
+				valid = false;// forces loop
 				again = " again ";
 			} else {
 				for (int i = 0; i < currectAnswer.length; i++) {// converts string to single char arrays
 					currectAnswer[i] = Character.toString(currentGuess.charAt(i));
 				}
 			}
-
-		} while (valid == false);
+		} while (valid == false);// loop in cases of invalid input
 		return currectAnswer;
 	}
 
 	/**
-	 * Checks if the guess the user has inputted is valid and follows the right
-	 * amount of code need to be written and the colors given.
+	 * Checks if the currentGuess is of the same size as SIZE and also only contains
+	 * the character found i the VALID_CHARS string.
 	 *
 	 * @param VALID_CHARS
-	 *            colors that the user would have to use when inputting their guess
-	 *            each character in the string represents a different color
+	 *            Colors that the user would have to use when inputting their guess
+	 *            each character in the string represents a different color.
 	 * @param SIZE
 	 *            The SIZE/length of the code that the user must guess
 	 * @param currentGuess
-	 *            Takes the guess and sees if it is valid to the rules
-	 * @return If the currentGuess contains one or more strings that VALID_CHARS
+	 *            An array with the users current guess with each color in a
+	 *            separate index of one character.
+	 * @return If the currentGuess contains one or more characters that VALID_CHARS
 	 *         does not have the method will return false. The method will also
 	 *         return false if the length of the currentGuess is not the same as the
 	 *         value of SIZE
@@ -184,14 +192,15 @@ public class code_Breaker {
 	 * correctly.
 	 *
 	 * @param gencode
-	 *            compares the generated code that the computer has made in random
-	 *            to the code the user has guessed
+	 *            The array gencode is an array that contains the randomly generated
+	 *            code that the user in trying to guess. Each index of the array
+	 *            holds one character that represents a color
 	 * @param currentGuess
 	 *            Takes the guess of the user and compares it with the generated
 	 *            code for the correct color and position
-	 * @return If the guess has none of the correct color or position then does does
-	 *         not return anything, and asks to guess again, if guess has any amount
-	 *         of colors and in the correct position will output a number abount of
+	 * @return If the guess has none of the correct color or position then the
+	 *         method returns an array with length 0 if guess has any amount of
+	 *         colors and in the correct position will output a number abount of
 	 *         correct with b indicating a black peg and that the position and color
 	 *         is correct.
 	 */
@@ -211,11 +220,14 @@ public class code_Breaker {
 	}
 
 	/**
-	 * Checks if the user's guess is the same length as the generated code.
+	 * If the index of currentGuess and the index of gencodeCOPY are the same then
+	 * set that index to "/" for currentGuess and "*" for gencodeCOPY. The correct
+	 * guesses must be changed so that the find color correct method does not count
+	 * these guesses again.
 	 *
-	 * @param gencode
-	 *            compares the generated code that the computer has made in random
-	 *            to the code the user has guessed
+	 * @param gencodeCPOY
+	 *            Compares the generated code that the computer has made in random
+	 *            to the code the user has guessed.
 	 * @param currentGuess
 	 *            Takes the guess of the user and compares it with the generated
 	 *            code to check for the same length of the generated code
@@ -234,18 +246,18 @@ public class code_Breaker {
 	}
 
 	/**
-	 * Checks if the user's guess have the correct color.
+	 * Checks to see if the user has the correct color guesses but in the wrong
+	 * position.
 	 *
 	 * @param gencode
-	 *            compares the generated code that the computer has made in random
-	 *            to the code the user has guessed
+	 *            Compares the generated code that the computer has made in random
+	 *            to the code the user has guessed.
 	 * @param currentGuess
 	 *            Takes the guess of the user and compares it with the generated
-	 *            code for the correct color
-	 * @return If the guess has none of the correct colors then does not return
-	 *         anything, and asks to guess again, if guess has any amount of colors
-	 *         correct it will output the number amount of correct with w indicating
-	 *         a white peg and that the color is correct.
+	 *            code for the correct color.
+	 * @return Returns an array containing the number of correct colors with each
+	 *         index set to "w". The size of the return array has a max value of
+	 *         SIZE, and a minimum size of 0.
 	 */
 	public static String[] findColourCorrect(String[] gencodeCOPY, String[] currentGuess) {// David
 		ArrayList<String> holderList = new ArrayList<String>();// used an arrayList because it is dynamic
@@ -269,6 +281,9 @@ public class code_Breaker {
 	}
 
 	/**
+	 * Display game combines all of the previous guesses and previous clues and
+	 * combines them into a string that can be return. Each letter is separated by a
+	 * space and the guesses and clues are separated by a tab.
 	 *
 	 * @param guessHistory
 	 * @param answerHistory
@@ -306,8 +321,8 @@ public class code_Breaker {
 	public static void rules(String VALID_CHARS, int SIZE, int TRIES) {
 		System.out.println("You get " + TRIES + " attempts at breaking the code. \n"
 				+ "If you can't discover it by then, then you lose that game. \n"
-				+ "But that's OK. You can get a new code and try again.\n\n" + "The Code Breaker code is made up of "
-				+ SIZE + " pegs  from " + VALID_CHARS.length() + " available colors.\n"
+				+ "But that's OK. You can get a new code and try again.\n\n" + "The Code Breaker code is made up of " + SIZE
+				+ " pegs  from " + VALID_CHARS.length() + " available colors.\n"
 				+ "These colors are symbolized as the following characters" + VALID_CHARS + ". \n"
 				+ "In addition b=black and w= white. To win you must place the  right marbles in the right order.\n"
 				+ "After each attempt you will be told how you  went.\n\n"
